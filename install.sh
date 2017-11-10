8926fe05d844198ef93bed2ecbe9ec69e82bfcb5
Diogo Felix <diogo2fex@gmail.com>
2017-11-10 01:33:58 -0200

    elixir install

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 while [[ $# -gt 0 ]]; do
     case $1 in
-        -t|--tmux)
-            tmux=true;;
+        -e|--elixr)
+            elixir=true;;
         -n| --nvim)
             nvimrc=true;;
         -z| --zsh)
@@ -31,7 +31,7 @@ function ask {
         fi
 
         echo -n "$1 [$prompt] "
-        read reply </dev/tty
+        read -r reply </dev/tty
 
         if [ -z "$reply" ]; then
             reply=$default
@@ -45,6 +45,19 @@ function ask {
     done
 }
 
+function install_elixir {
+  mkdir "$HOME/.elixir" && cd "$HOME/.elixir"
+  echo "[ elixir ] downloading"
+  curl -L https://github.com/elixir-lang/elixir/releases/download/v1.5.2/Precompiled.zip > Precompiled.zip
+  unzip Precompiled.zip && rm Precompiled.zip && cd bin
+  echo "[ elixir ] fixing binaries"
+  termux-fix-shebang elixir elixirc iex mix
+  echo 'export PATH="$PATH:$HOME/.elixir/bin"' >> "$HOME/.profile"
+  cd "$HOME"
+  source "$HOME/.profile"
+  echo "[ elixir ] finished"
+}
+
 clear
 
 if ! [ -x "$(command -v git)" ]; then
@@ -54,14 +67,14 @@ fi
 
 if [ -d "$HOME/.termux" ]; then
   if ask "[ termux ] configs found, overwrite?" Y; then
-    rm -rf $HOME/.termux
-    curl -fsLo $HOME/.termux/colors.properties --create-dirs https://cdn.rawgit.com/onlurking/termux/master/.termux/colors.properties
-    curl -fsLo $HOME/.termux/font.ttf --create-dirs https://cdn.rawgit.com/onlurking/termux/master/.termux/font.ttf
+    rm -rf "$HOME/.termux"
+    curl -fsLo "$HOME/.termux/colors.properties" --create-dirs https://cdn.rawgit.com/onlurking/termux/master/.termux/colors.properties
+    curl -fsLo "$HOME/.termux/font.ttf" --create-dirs https://cdn.rawgit.com/onlurking/termux/master/.termux/font.ttf
   fi
 else
   echo "[ termux ] downloading configs"
-  curl -fsLo $HOME/.termux/colors.properties --create-dirs https://cdn.rawgit.com/onlurking/termux/master/.termux/colors.properties
-  curl -fsLo $HOME/.termux/font.ttf --create-dirs https://cdn.rawgit.com/onlurking/termux/master/.termux/font.ttf
+  curl -fsLo "$HOME/.termux/colors.properties" --create-dirs https://cdn.rawgit.com/onlurking/termux/master/.termux/colors.properties
+  curl -fsLo "$HOME/.termux/font.ttf" --create-dirs https://cdn.rawgit.com/onlurking/termux/master/.termux/font.ttf
 fi
 
 if [ $zsh ];then
@@ -71,23 +84,40 @@ if [ $zsh ];then
     fi
     if [ ! -d "$HOME/.oh-my-zsh" ]; then
         echo "[ oh-my-zsh ] clonning repository"
-        git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1 --quiet > /dev/null
+        git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1 --quiet > /dev/null
     else
       if ask "[ oh-my-zsh ] configs found, overwrite?" Y; then
-          rm -rf $HOME/.oh-my-zsh
-          git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1 --quiet > /dev/null
+          rm -rf "$HOME/.oh-my-zsh"
+          git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1 --quiet > /dev/null
       fi
     fi
-    curl -fsLo $HOME/.oh-my-zsh/themes/lambda-mod.zsh-theme https://cdn.rawgit.com/onlurking/termux/master/.termux/lambda-mod.zsh-theme
-    curl -fsLo $HOME/.zshrc https://cdn.rawgit.com/onlurking/termux/master/.termux/.zshrc
-    curl -fsLo $HOME/.profile https://cdn.rawgit.com/onlurking/termux/master/.termux/.profile
+    curl -fsLo "$HOME/.oh-my-zsh/themes/lambda-mod.zsh-theme" https://cdn.rawgit.com/onlurking/termux/master/.termux/lambda-mod.zsh-theme
+    curl -fsLo "$HOME/.zshrc" https://cdn.rawgit.com/onlurking/termux/master/.termux/.zshrc
+    curl -fsLo "$HOME/.profile" https://cdn.rawgit.com/onlurking/termux/master/.termux/.profile
     if ask "[ oh-my-zsh ] enable syntax highlighting?" Y; then
       echo "[ oh-my-zsh ] downloading plugin"
-      git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting --quiet > /dev/null
+      git clone git://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting" --quiet > /dev/null
     else
-      sed -i '4s/.*/plugins=(git)/' $HOME/.zshrc
+      sed -i '4s/.*/plugins=(git)/' "$HOME/.zshrc"
     fi
     chsh -s zsh
+fi
+
+if [ $elixir ];then
+  if ! [ -x "$(command -v erlang)" ]; then
+    echo "[ erlang ] not found, installing"
+    apt-get install -y erlang > /dev/null 2>&1
+  fi
+  
+fi
+
+if [ ! -d "$HOME/.elixir" ]; then
+  install_elixir
+else
+  if ask "[ elixir ] found, overwrite?" Y; then
+    rm -rf "$HOME/.elixir"
+    install_elixir
+  fi
 fi
 
 if ask "[ storage ] setup external storage?" Y; then
@@ -95,7 +125,7 @@ if ask "[ storage ] setup external storage?" Y; then
 fi
 
 if ask "[ termux ] hide welcome message?" Y; then
-    touch $HOME/.hushlogin
+    touch "$HOME/.hushlogin"
 fi
 
 if ask "[ finished ] close termux?" Y; then
